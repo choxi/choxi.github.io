288a5e2c5d8cfa281b25359b823b5a7eaa49f9e8
Roshan Choxi <roshan.choxi@gmail.com>
2021-05-01 10:59:32 -0500

    Allows multiple pages

--- totem.js
+++ totem.js
@@ -1,17 +1,20 @@
 const esbuild = require("esbuild")
 const fs = require("fs")
 const pascalCase = require("pascalcase")
-const HTML_TEMPLATE = `
+
+function template(page) {
+  return `
 <html>
   <head>
     <link rel="stylesheet" href="../assets/application.scss">
     <script src="./index.js"></script>
   </head>
   <body>
-    <div id="site"></div>
+    <div id="${ page }"></div>
   </body>
 </html>
 `
+}
 
 console.log("Creating dist directory...")
 fs.rmdirSync("./dist", { recursive: true, force: true })
@@ -34,7 +37,36 @@ const importStatements = componentConfigs.map(config => {
   return `import ${ config.klass } from "../components/${ config.path }"`
 })
 
-const inFile = fs.readFileSync("./posts/demo.html")
+const pagePaths = fs.readdirSync("./pages")
+const pageConfigs = pagePaths.map(path => {
+  const name = path.split(".")[0]
+  const klass = pascalCase(name)
+  const body = fs.readFileSync(`./pages/${ path }`)
+
+  return {
+    path: path,
+    klass: klass,
+    body: body
+  }
+})
+
+const pageComponents = pageConfigs.map(page => {
+  return `
+    class ${ page.klass } extends React.Component {
+      render() {
+        return (
+          ${ page.body }
+        )
+      }
+    }
+
+    document.addEventListener('DOMContentLoaded', (event) => {
+      const container = document.getElementById("${ page.klass }")
+      if (container) ReactDOM.render(<${ page.klass } />, container)
+    })
+  `
+})
+
 
 const componentsEntrypoint = `
   import React from "react"
@@ -42,17 +74,7 @@ const componentsEntrypoint = `
 
   ${ importStatements.join("\n") }
 
-  class Site extends React.Component {
-    render() {
-      return (
-        ${ inFile }
-      )
-    }
-  }
-
-  document.addEventListener('DOMContentLoaded', (event) => {
-    ReactDOM.render(<Site />, document.getElementById("site"))
-  })
+  ${ pageComponents.join("\n") }
 `
 
 fs.writeFileSync("./dist/_index.jsx", componentsEntrypoint)
@@ -63,11 +85,14 @@ esbuild.buildSync({
   outfile: "./dist/index.js"
 })
 
-fs.writeFileSync("./dist/index.html", HTML_TEMPLATE)
+pageConfigs.forEach(page => {
+  fs.writeFileSync(`./dist/${ page.path }`, template(page.klass))
+})
 
 console.log("Site generated")
 
 // TODO
 // - [ ] Support SASS
-// - [ ] Output all pages
+// - [X] Allow multiple pages
+// - [ ] Allow third-party modules (Helmet)
 
