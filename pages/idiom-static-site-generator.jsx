f72ea0684e24c6faba3526b54d0a6e3dbec09b46
Roshan Choxi <roshan.choxi@gmail.com>
2021-05-09 20:02:16 -0500

    Add to idiomjs post

--- pages/idiom-static-site-generator.jsx
+++ pages/idiom-static-site-generator.jsx
@@ -1,7 +1,7 @@
 <PostLayout>
   <h1>Idiom: A Simple Static Site Generator in React</h1>
   <p>
-    One of my favorite design choices of React is that it extends HTML and allows you to define your own elements. Using React (or Vue, Web Components, etc.) you can build your own markup language however you like and create rich, interactive web pages. I wanted to create my own type of markup language for this personal site so that I could write posts like:
+    One of my favorite aspects of React is that it extends HTML and allows you to define your own elements. Using React (or Vue, Web Components, etc.) you can build your own markup language however you like and create rich, interactive web pages. I wanted to create my own markup for this personal site so that I could write posts like:
   </p>
 
   <Code lang="jsx">
@@ -13,19 +13,23 @@
           $ idiomjs build
         </Snippet>
 
-        <Subscribe />
+        <Subscribe newsletter="idiom" cta="Get updates on Idiom" />
       </Post>
     `}
   </Code>
 
   <p>
-    A popular framework for this type of React-based static site is GatsbyJS. Gatsby is powerful but complicated, it depends on GraphQL by default and. This seems like overkill for a static site generator, I wanted something that:
+    A popular framework for this type of React-based static site is GatsbyJS. Gatsby is powerful but complex, it requires a lot of configuration, can be difficult to debug, and uses Webpack and GraphQL in its toolchain. This seems like overkill for a static site generator, I wanted something that:
   </p>
 
   <ul>
     <li>Has an extendable markup language</li>
     <li>Is search-engine friendly</li>
-    <li>Has a minimal number of dependencies</li>
+    <li>Has a simple toolchain with a minimal number of dependencies</li>
     <li>Can easily be deployed to any static site host (e.g. GitHub Pages or S3)</li>
   </ul>
+
+  <p>
+    In contrast, Idiom uses the very fast <a>esbuild</a> to compile your JS and <a>jsdom</a> for prerendering. It also allows you to write your pages as a pure JSX expression without any import boilerplate by autoloading your components assuming certain naming conventions.
+  </p>
 </PostLayout>
