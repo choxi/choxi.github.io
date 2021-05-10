5157aa3a482b56816f89812f14b1465c4bf985e9
Roshan Choxi <roshan.choxi@gmail.com>
2021-05-09 20:46:17 -0500

    Adds google analytics

--- components/post-layout.jsx
+++ components/post-layout.jsx
@@ -1,11 +1,25 @@
 import React from "react"
 import Sidebar from "./sidebar"
+import Helmet from "react-helmet"
 
 import "./application.scss"
 
 export default function PostLayout(props) {
   return (
     <div className="layout-sidebar">
+      <Helmet>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-86991778-1"></script>
+        <script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-86991778-1');
+          `}
+        </script>
+      </Helmet>
+
       <div className="layout-sidebar--sidebar pad pad-ew-l sm-hide lg-show">
         <Sidebar />
       </div>
