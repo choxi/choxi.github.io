404e0b2fe222651197ea24ff077eb10efac7f362
Roshan Choxi <roshan.choxi@gmail.com>
2021-05-18 19:46:32 -0500

    Adds caption

--- pages/three-body-problems.jsx
+++ pages/three-body-problems.jsx
@@ -5,28 +5,31 @@
   <div className="post">
     <h1 className="spc-n-zero"> Three Body Problems </h1>
 
+
+    <p>
+      The <a href="https://en.wikipedia.org/wiki/Three-body_problem">Three Body Problem</a> is a famous problem in physics. For any two orbiting bodies, we can predict their exact position and trajectory at any time in the future using Newton's Law of Gravitation. But if we introduce a third body, there is no known formula to predict the trajectory of each body into the future. We can still figure it out, but not formulaically. The only way to do it is to run a simulation where we start with the initial mass and velocity of each body, calculate the forces on each, move them one step forward, and repeat.
+    </p>
+
     <Orbits />
 
-    <p>
-      The <a href="https://en.wikipedia.org/wiki/Three-body_problem">Three Body Problem</a> is a famous problem in physics. For any two orbiting bodies, we can calculate their exact position and trajectory at any point in the future analytically (i.e. with a formula). But if we introduce a third body into the scenario, there is no known equation (i.e. a closed form or analytic solution) to predict the trajectory of each body into the future. The only way to do it is to run a simulation where we have an initial condition, calculate the forces on each, move them forward one step, and calculate again.
-    </p>
-
-    {/* <!-- explanation of analytical vs numerical solutions-- > */}
+    <div className="caption">
+      A simulation of the "Figure 8", one of the few known stable three body orbits.
+    </div>
 
     <p>
-      It’s fascinating that the problem takes such a leap in difficulty by adding one more body. We can dervice a formula for two bodies just with a few simple Newton’s Laws. But when we need to apply them to a model with N > 2 bodies, we can’t figure it out without simulating it.
-    </p>
-
-    <p>
-      How many problems are there like the three body problem?
+      Newton published his law of gravitation and effectively solved the "Two Body Problem" in 1686, but the problem takes such a leap in difficulty with any more bodies that we haven't solved it in the ~350 years since then. The challenge is well described in the plot of <a href="https://en.wikipedia.org/wiki/The_Three-Body_Problem_(novel)">The Three-Body Problem</a>, where an advanced alien civilization goes through repeated near-extinction events because of their inability to predict their planet's chaotic orbit in a system with three stars.
     </p>
 
     <p>
-      Genetics may be in the same category. We sequenced the full genome in 2003 but have found that it’s still much more complicated to predict how individual gene changes will affect an organism. Even if we knew exactly what a gene does in isolation and how it responds to different biochemistry, can we predict what will happen with two or three genes if they affect each other? We might know how individual bacteria behave, but how do we model the microbiome? You can imagine the same problem in predicting the behavior of a biosphere, an economy, condensed matter, or any other situation where we have agents that follow simple rules individually but result in complex behavior when taken as a whole.
+      The Three Body Problem is a great example of a system that follows very basic rules individually (there's only one rule, the law of gravitation), but explodes in complexity when we add more units. How many problems are there like the Three Body Problem?
     </p>
 
     <p>
-      Understanding how many problems there are that fit into this category has important implications on scientific and technological progress. Patrick Collison calls these <a href="https://noahpinion.substack.com/p/interview-patrick-collison-co-founder">Wicked Problems</a> and describes them as one possible factor in recent scientific stagnation:
+      Genetics is one example that comes to mind. We sequenced the full human genome in 2003 and have some understanding of the rules of individual gene expression, but we still have no predictive understanding of the a genome as a whole. In economics, we can predict the behavior of prototypical consumers in one market, but we know very little about how different markets interact on the macro scale. We could fully catalogue the trillions of bacteria in our guts, but would we understand how the entire microbiome behaves? You can imagine the same problem in predicting the behavior of a biosphere, climate change, condensed matter, or any other situation where we have that follow simple rules individually but result in complex behavior when taken as a whole.
+    </p>
+
+    <p>
+      Understanding how many problems there are that fit into this category has important implications on scientific and technological progress. Patrick Collison has a similar concept he calls <a href="https://noahpinion.substack.com/p/interview-patrick-collison-co-founder">Wicked Problems</a> and describes them as one possible factor in recent scientific stagnation:
     </p>
 
     <blockquote>
@@ -34,7 +37,11 @@
     </blockquote>
 
     <p>
-      Stephen Wolfram calls them <a href="https://en.wikipedia.org/wiki/Computational_irreducibility">computationally irreducible</a> problems and believes they might imply the future is fundamentally unpredictable. Imagine that one day we did discover a Universal Theory of Everything. It’s just one simple rule or equation, and with it we can explain the emergence of every particle, field, or force from the very beginning of the Big Bang. How much would we actually be able to <i>predict</i> though? If we knew the state of every atom in the galaxy, we may still have to simulate it to predict how its stars and planets evolve. Instead of deriving theories with mathematical formula, maybe scientists in the future will be huddled around a simulation of the solar system tinkering with the configs to see how mankind emerged.
+      Stephen Wolfram calls them <a href="https://en.wikipedia.org/wiki/Computational_irreducibility">computationally irreducible problems </a> and believes they might imply the future is fundamentally unpredictable. Imagine that one day we did discover a Universal Theory of Everything. It’s just one simple rule, and with it we can explain the emergence of every particle, field, or force from the very beginning of the Big Bang. How much would we actually be able to <i>predict</i> though? If we knew the state of every atom in the galaxy, we may still have to simulate it to predict how its stars and planets will evolve.
+    </p>
+
+    <p>
+      Maybe understanding the basic rules of physics and mathematics isn't even the hard part, and advanced civilizations ultimately have to rely on simulations to make technological progress. In addition to <a href="https://en.wikipedia.org/wiki/Simulation_hypothesis#The_simulation_hypothesis_in_physics">all the weird computer-like aspects of the universe</a>, I count this as one more point in favor of the <a href="https://en.wikipedia.org/wiki/Simulation_hypothesis">Simulation Hypothesis</a>.
     </p>
   </div>
 </PostLayout>
