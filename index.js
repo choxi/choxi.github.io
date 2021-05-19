69564f8918fcfa4f8253e09bf4bb324408216b50
Roshan Choxi <roshan.choxi@gmail.com>
2021-05-18 21:54:08 -0500

    Updates

--- index.js
+++ index.js
@@ -42180,6 +42180,16 @@ For more info, visit https://reactjs.org/link/mock-scheduler`);
   var Y_AXIS = 1;
   var X_AXIS = 2;
   var Utils = class {
+    static isSafari() {
+      const ua = navigator.userAgent.toLowerCase();
+      if (ua.indexOf("safari") != -1) {
+        if (ua.indexOf("chrome") > -1) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
     static debugBezier(p, a1, c1, c2, a2) {
       p.push();
       p.noFill();
@@ -43287,18 +43297,26 @@ For more info, visit https://reactjs.org/link/mock-scheduler`);
           new Body(2, x2, v2, 1, color_default.palettes.flat.amethyst),
           new Body(3, x3, v3, 1, color_default.palettes.flat.peterRiver)
         ];
-        let points = [];
         let t = -1;
         const deltaT = 0.01;
+        let trace;
+        const safari = utils_default.isSafari();
         p.setup = () => {
           p.createCanvas(width, height, p.WEBGL);
           p.fill(255, 0, 0);
           p.noStroke();
+          trace = p.createGraphics(width, height, p.WEBGL);
+          trace.fill(255, 0, 0);
+          trace.noStroke();
         };
         p.draw = () => {
           t += 1;
           p.clear(0);
-          points.forEach((point) => point.render(p));
+          if (safari)
+            p.scale(1, -1);
+          p.image(trace, -width / 2, -height / 2);
+          if (safari)
+            p.scale(1, -1);
           bodies.forEach((body) => body.render(p));
           const newBodies = [];
           bodies.forEach((body) => {
@@ -43319,14 +43337,10 @@ For more info, visit https://reactjs.org/link/mock-scheduler`);
             } else {
               newBodies.push(new Body(body.id, newPosition, newVelocity, body.mass, body.color));
             }
-            if (t % 5 === 0) {
-              points.push(new Point(newPosition.multiply(200), body.color));
-            }
+            const point = new Point(newPosition.multiply(200), body.color);
+            point.render(trace);
           });
           bodies = newBodies;
-          if (points.length > 1e3) {
-            points = points.slice(1, points.length);
-          }
         };
       }), /* @__PURE__ */ import_react9.default.createElement("div", {
         className: "caption"
@@ -43770,13 +43784,13 @@ For more info, visit https://reactjs.org/link/mock-scheduler`);
         className: "spc-n-zero"
       }, " Three Body Problems "), /* @__PURE__ */ import_react14.default.createElement("p", null, "The ", /* @__PURE__ */ import_react14.default.createElement("a", {
         href: "https://en.wikipedia.org/wiki/Three-body_problem"
-      }, "Three Body Problem"), " is a famous problem in physics. For any two orbiting bodies, we can predict their exact position and trajectory at any time in the future using Newton's Law of Gravitation. But if we introduce a third body, there is no known formula to predict the trajectory of each body into the future. We can still figure it out, but not formulaically. The only way to do it is to run a simulation where we start with the initial mass and velocity of each body, calculate the forces on each, move them one step forward, and repeat."), /* @__PURE__ */ import_react14.default.createElement(orbits_default, null), /* @__PURE__ */ import_react14.default.createElement("p", null, `Newton published his law of gravitation and effectively solved the "Two Body Problem" in 1686, but the problem takes such a leap in difficulty with any more bodies that we haven't solved it in the ~350 years since then. The challenge is well described in the plot of `, /* @__PURE__ */ import_react14.default.createElement("a", {
+      }, "Three Body Problem"), " is a famous problem in physics. For any two orbiting bodies, we can predict their exact position and trajectory at any time in the future using Newton's Law of Gravitation. But if we introduce a third body, there is no known formula to predict the trajectory of each body into the future. We can still figure it out, but not formulaically. The only way to do it is to run a simulation where we start with the initial mass and velocity of each body, calculate the forces on each, move them one step forward, and repeat."), /* @__PURE__ */ import_react14.default.createElement(orbits_default, null), /* @__PURE__ */ import_react14.default.createElement("p", null, `Newton published his law of gravitation and effectively solved the "Two Body Problem" in 1686, but the problem takes such a leap in difficulty with any more bodies that we haven't solved it for > 2 bodies in the ~350 years since then. The challenge is well described in the plot of `, /* @__PURE__ */ import_react14.default.createElement("a", {
         href: "https://en.wikipedia.org/wiki/The_Three-Body_Problem_(novel)"
-      }, "The Three-Body Problem"), ", where an advanced alien civilization goes through repeated near-extinction events because of their inability to predict their planet's chaotic orbit in a system with three stars."), /* @__PURE__ */ import_react14.default.createElement("p", null, "The Three Body Problem is a great example of a system that follows very basic rules individually (there's only one rule, the law of gravitation), but explodes in complexity when we add more units. How many problems are there like the Three Body Problem?"), /* @__PURE__ */ import_react14.default.createElement("p", null, "Genetics is one example that comes to mind. We sequenced the full human genome in 2003 and have some understanding of the rules of individual gene expression, but we still have no predictive understanding of the a genome as a whole. In economics, we can predict the behavior of prototypical consumers in one market, but we know very little about how different markets interact on the macro scale. We could fully catalogue the trillions of bacteria in our guts, but would we understand how the entire microbiome behaves? You can imagine the same problem in predicting the behavior of a biosphere, climate change, condensed matter, or any other situation where we have that follow simple rules individually but result in complex behavior when taken as a whole."), /* @__PURE__ */ import_react14.default.createElement("p", null, "Understanding how many problems there are that fit into this category has important implications on scientific and technological progress. Patrick Collison has a similar concept he calls ", /* @__PURE__ */ import_react14.default.createElement("a", {
+      }, "The Three-Body Problem"), ", where an advanced alien civilization goes through repeated near-extinction events because of their inability to predict their planet's chaotic orbit in a system with three stars."), /* @__PURE__ */ import_react14.default.createElement("p", null, "The Three Body Problem is a great example of a system that follows very basic rules individually (there's only one rule, the law of gravitation), but explodes in complexity when we add more units. How many problems are there like the Three Body Problem?"), /* @__PURE__ */ import_react14.default.createElement("p", null, "Genetics is one example that comes to mind. We sequenced the full human genome in 2003 and have some understanding of the rules of individual gene expression, but we still have no predictive understanding of the genome as a whole. In economics, we can predict the behavior of prototypical consumers in one market, but we know very little about how different markets interact on the macro scale. We could fully catalogue the trillions of bacteria in our guts, but would we understand how the entire microbiome behaves? You can imagine the same problem in predicting the behavior of a biosphere, climate change, condensed matter, or any other scenario that follows simple rules at a unit level but results in complex behavior when taken as a whole."), /* @__PURE__ */ import_react14.default.createElement("p", null, "Understanding how many problems there are that fit into this category has important implications on scientific and technological progress. Patrick Collison has a similar concept he calls ", /* @__PURE__ */ import_react14.default.createElement("a", {
         href: "https://noahpinion.substack.com/p/interview-patrick-collison-co-founder"
       }, "Wicked Problems"), " and describes them as one possible factor in recent scientific stagnation:"), /* @__PURE__ */ import_react14.default.createElement("blockquote", null, `...the major open problems in many domains involve emergent phenomena and complex/unstable systems that often have lots of complex couplings and nonlinear effects and so on. In biology, cancer, autoimmune conditions, anything involving the microbiome... these are all just intrinsically harder problems than individual infectious diseases. In computing, modern machine learning is much more about experimentally figuring out what works in an emergent sense than, say, operating system or network protocol design, which are more about top-down architecture. ...You could probably extend the argument to materials science or condensed matter physics... these aren't as neatly characterizable in closed forms as, say, basic mechanics or thermodynamics. It's hard to say whether the proportion of important problems that are "wicked" has increased but I think it's plausible that it has.`), /* @__PURE__ */ import_react14.default.createElement("p", null, "Stephen Wolfram calls them ", /* @__PURE__ */ import_react14.default.createElement("a", {
         href: "https://en.wikipedia.org/wiki/Computational_irreducibility"
-      }, "computationally irreducible problems "), " and believes they might imply the future is fundamentally unpredictable. Imagine that one day we did discover a Universal Theory of Everything. It\u2019s just one simple rule, and with it we can explain the emergence of every particle, field, or force from the very beginning of the Big Bang. How much would we actually be able to ", /* @__PURE__ */ import_react14.default.createElement("i", null, "predict"), " though? If we knew the state of every atom in the galaxy, we may still have to simulate it to predict how its stars and planets will evolve."), /* @__PURE__ */ import_react14.default.createElement("p", null, "Maybe understanding the basic rules of physics and mathematics isn't even the hard part, and advanced civilizations ultimately have to rely on simulations to make technological progress. In addition to ", /* @__PURE__ */ import_react14.default.createElement("a", {
+      }, "computationally irreducible problems "), ` and believes they might imply the future is fundamentally unpredictable if we can't find "pockets of reducibility" that give us shortcuts to a full simulation. Imagine that one day we did discover a Universal Theory of Everything: it\u2019s just one simple rule, and with it we can explain the emergence of every particle, field, or force from the very beginning of the Big Bang. How much would we actually be able to `, /* @__PURE__ */ import_react14.default.createElement("i", null, "predict"), " though? If we knew the state of every atom in the galaxy, we may still have to simulate it to predict how its stars and planets will evolve."), /* @__PURE__ */ import_react14.default.createElement("p", null, "Maybe understanding the basic rules of physics and mathematics isn't even the hard part, and advanced civilizations ultimately have to rely on simulations to make technological progress. In addition to ", /* @__PURE__ */ import_react14.default.createElement("a", {
         href: "https://en.wikipedia.org/wiki/Simulation_hypothesis#The_simulation_hypothesis_in_physics"
       }, "all the weird computer-like aspects of the universe"), ", I count this as one more point in favor of the ", /* @__PURE__ */ import_react14.default.createElement("a", {
         href: "https://en.wikipedia.org/wiki/Simulation_hypothesis"
